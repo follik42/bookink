82dc3ed784db102f51c17e47abb3a4106b6661bc
toron2c <toron2c@yandex.ru>
2023-01-25 15:24:07 +0300

    2

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,29 @@ import { renderSearchStubBlock } from './search-results.js';
 import { renderUserBlock } from './user.js';
 import { renderToast } from './lib.js';
 
+/**
+ * function getUserdata from localstorage
+ * @returns {username: unknown, avatarUrl:unknown}
+ */
+function getUserData(): { username: unknown, avatarUrl: unknown } {
+
+  const username = window.localStorage.getItem('username');
+  const avatarUrl = window.localStorage.getItem('avatarUrl');
+
+  return { username, avatarUrl }
+}
+
+/**
+ * function getFavoritesUserAmount from localstorage
+ * @returns countFavorites:unknown
+ */
+function getFavoritesAmount(): unknown {
+  const count = window.localStorage.getItem('favoritesAmount');
+
+  return count;
+}
+
+
 window.addEventListener('DOMContentLoaded', () => {
   renderUserBlock('name', '5', 6);
   renderSearchFormBlock();
@@ -20,3 +43,7 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   );
 });
+
+
+
+
